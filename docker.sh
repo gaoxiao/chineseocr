fc0c821a720444d426db48afb1eaf8917abd31e1
Xiao Gao <gaoxiao1988@gmail.com>
2019-11-24 16:37:43 -0800

    Docker env works.

--- docker.sh
+++ docker.sh
@@ -3,4 +3,24 @@ docker build -t chineseocr .
 ##启动服务
 docker run -d -p 8080:8080 chineseocr /root/anaconda3/bin/python app.py
 
+docker run --runtime=nvidia \
+-e NVIDIA_VISIBLE_DEVICES=1 \
+-d --rm \
+-p 8080:8080 \
+--shm-size=2g \
+--name chineseocr \
+chineseocr \
+/root/anaconda3/bin/python app.py
 
+
+
+docker run --runtime=nvidia \
+-it \
+-e NVIDIA_VISIBLE_DEVICES=1 \
+-v ~/code/chineseocr:/code \
+--rm \
+-p 8080:8080 \
+--shm-size=2g \
+--name chineseocr \
+chineseocr \
+/bin/bash
